960c4a2b6f048390b8d097eb98795709fce81374
Utkarsh Sharma <skhiearth@gmail.com>
2020-02-29 02:43:07 +0530

    safe division using SafeMath library

--- Source/6-Libraries_Math/6-Libraries_Math.sol
+++ Source/6-Libraries_Math/6-Libraries_Math.sol
@@ -2,11 +2,15 @@ pragma solidity ^0.5.1;
 
 // Import library from different file
 import "Source/6-Libraries_Math/Math.sol";
+import "Source/6-Libraries_Math/SafeMath.sol";
 
 contract MyContract {
+    using SafeMath for uint256;
     uint256 public value;
+    uint256 public safeval;
 
     function calc(uint256 _value1, uint256 _value2) public {
         value = Math.divide(_value1, _value2);
+        safeval = _value1.div(_value2);
     }
 }
